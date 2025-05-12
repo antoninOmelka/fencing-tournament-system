b2a08aea6a64aba0250b25b45124af4b79cabdaa
Antonin Omelka <antonin.omelka@email.cz>
2025-05-12 10:29:02 +0200

    Participant edit logic moved to ParticipantTable

--- app/components/ParticipantsTable/ParticipantsTable.tsx
+++ app/components/ParticipantsTable/ParticipantsTable.tsx
@@ -1,65 +1,136 @@
 import "./../../styles/global/global.css";
 
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Table, TableBody, TableHead, TableRow, Paper } from "@mui/material";
 import { StyledTableContainer, StyledTableCell } from "../../styles/shared/tables";
 import { Participant } from "../../types/participant";
 import ParticipantsTableRow from "../ParticipantRow/ParticipantsTableRow";
+import { StyledButton } from "@/app/styles/shared/buttons";
+import { z } from "zod";
+import { deleteParticipant, updateParticipant } from "@/app/services/participants";
 
 type ParticipantsTableProps = {
-  participants: Participant[];
-  onDeleteParticipant: (id: number) => void;
-  onEditParticipant: (id: number) => void;
-  editingId: number | null;
-  newParticipant: Omit<Participant, "id">;
-  onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onSaveEdit: () => void;
-  errors: Record<string, string>;
+  inicialParticipants: Participant[];
 };
 
-function ParticipantsTable({
-  participants,
-  onEditParticipant,
-  onDeleteParticipant,
-  editingId,
-  newParticipant,
-  onInputChange,
-  onSaveEdit,
-  errors
-}: ParticipantsTableProps) {
+const participantSchema = z.object({
+  name: z.string().min(1).max(25),
+  year: z.coerce.number().min(1900).max(2025).refine(val => Number.isInteger(val)),
+  club: z.string().min(1).max(25),
+  ranking: z.coerce.number().min(1).max(999).refine(val => Number.isInteger(val)),
+});
+
+function ParticipantsTable({ inicialParticipants }: ParticipantsTableProps) {
+  const [participants, setParticipants] = useState<Participant[]>(inicialParticipants);
+  const [newParticipant, setNewParticipant] = useState({ name: "", year: "", club: "", ranking: '' });
+  const [editingId, setEditingId] = useState<number | null>(null);
+  const [errors, setErrors] = useState<Record<string, string>>({});
 
   const isActionDisabled = Boolean(editingId);
-  
+
+  const handleInputChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNewParticipant(prev => ({ ...prev, [name]: value }));
+  }, []);
+
+  const handleAddParticipant = useCallback(() => {
+    const participant: Participant = {
+      id: Date.now(), // Use timestamp as a simple unique id
+      ...newParticipant
+    };
+    setEditingId(participant.id);
+    setParticipants(prev => [participant, ...prev]);
+    setNewParticipant({ name: "", year: "", club: "", ranking: "" });
+    setErrors({});
+  }, [newParticipant, setParticipants]);
+
+  const handleEditParticipant = useCallback((id: number) => {
+    setEditingId(id);
+    const participantToEdit = participants.find(p => p.id === id);
+    if (participantToEdit) {
+      setNewParticipant(participantToEdit);
+    }
+  }, [participants]);
+
+  const handleSaveEdit = useCallback(async () => {
+    try {
+      const updatedParticipants = participants.map(p =>
+        p.id === editingId ? { ...newParticipant, id: p.id } : p
+      );
+
+      const participantToSave = updatedParticipants.find(p => p.id === editingId);
+      if (participantToSave) {
+        participantSchema.parse(participantToSave);
+        await updateParticipant(participantToSave);
+      }
+
+      setParticipants(updatedParticipants);
+      setEditingId(null);
+      setNewParticipant({ name: "", year: "", club: "", ranking: "" });
+      setErrors({});
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const newErrors = error.errors.reduce((acc, err) => {
+          acc[err.path.join(".")] = err.message;
+          return acc;
+        }, {} as Record<string, string>);
+        setErrors(newErrors);
+      } else {
+        console.error(error);
+      }
+    }
+  }, [editingId, newParticipant, participants]);
+
+  const handleDeleteParticipant = useCallback(async (id: number) => {
+    try {
+      await deleteParticipant(String(id));
+      const updatedParticipants = participants.filter(participant => participant.id !== id);
+      setParticipants(updatedParticipants);
+    } catch (error) {
+      console.error(error);
+      throw new Error("Failed to delete participant");
+    }
+  }, [participants]);
+
   return (
-    <StyledTableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell className="name">Name</StyledTableCell>
-            <StyledTableCell className="year">Year</StyledTableCell>
-            <StyledTableCell className="club">Club</StyledTableCell>
-            <StyledTableCell className="ranking">Ranking</StyledTableCell>
-            <StyledTableCell className="actions">Actions</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {participants.map((participant) => (
+    <>
+      <div className="table-button-container">
+        <StyledButton variant="contained" onClick={handleAddParticipant} disabled={editingId ? true : false}>
+          Add New
+        </StyledButton>
+      </div>
+
+      <StyledTableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell className="name">Name</StyledTableCell>
+              <StyledTableCell className="year">Year</StyledTableCell>
+              <StyledTableCell className="club">Club</StyledTableCell>
+              <StyledTableCell className="ranking">Ranking</StyledTableCell>
+              <StyledTableCell className="actions">Actions</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {participants.map((participant) => (
               <ParticipantsTableRow
                 key={participant.id}
                 participant={participant}
                 isEditing={editingId === participant.id}
                 newParticipant={newParticipant}
-                onInputChange={onInputChange}
-                onSaveEdit={onSaveEdit}
-                onEditParticipant={onEditParticipant}
-                onDeleteParticipant={onDeleteParticipant}
+                onInputChange={handleInputChange}
+                onSaveEdit={handleSaveEdit}
+                onEditParticipant={handleEditParticipant}
+                onDeleteParticipant={handleDeleteParticipant}
                 isActionDisabled={isActionDisabled}
                 errors={errors}
               />
-          ))}
-        </TableBody>
-      </Table>
-    </StyledTableContainer>
+            ))}
+          </TableBody>
+        </Table>
+      </StyledTableContainer>
+    </>
+
   );
 }
 
--- app/participants/page.tsx
+++ app/participants/page.tsx
@@ -3,28 +3,18 @@
 import "@/app/styles/global/global.css";
 
 import React, { useState, useMemo, useEffect, useCallback } from "react";
-import { deleteParticipant, getParticipants, updateParticipant } from "../services/participants";
+import { getParticipants } from "../services/participants";
 import ParticipantsTable from "../components/ParticipantsTable/ParticipantsTable";
 import Loading from "../components/Loading/Loading";
 import { Participant } from "../types/participant";
 import { StyledButton } from "../styles/shared/buttons";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
-import { z } from "zod";
-
-const participantSchema = z.object({
-  name: z.string().min(1).max(25),
-  year: z.coerce.number().min(1900).max(2025).refine(val => Number.isInteger(val)),
-  club: z.string().min(1).max(25),
-  ranking: z.coerce.number().min(1).max(999).refine(val => Number.isInteger(val)),
-});
 
 function ParticipantsView() {
   const [participants, setParticipants] = useState<Participant[]>([]);
-  const [newParticipant, setNewParticipant] = useState({ name: "", year: "", club: "", ranking: '' });
-  const [editingId, setEditingId] = useState<number | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [errors, setErrors] = useState<Record<string, string>>({});
+
 
   useEffect(() => {
     async function fetchParticipants() {
@@ -41,70 +31,6 @@ function ParticipantsView() {
     fetchParticipants();
   }, []);
 
-  const handleInputChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setNewParticipant(prev => ({ ...prev, [name]: value }));
-  }, []);
-
-  const handleAddParticipant = useCallback(() => {
-    const participant: Participant = {
-      id: Date.now(), // Use timestamp as a simple unique id
-      ...newParticipant
-    };
-    setEditingId(participant.id);
-    setParticipants(prev => [participant, ...prev]);
-    setNewParticipant({ name: "", year: "", club: "", ranking: "" });
-    setErrors({});
-  }, [newParticipant]);
-
-  const handleEditParticipant = useCallback((id: number) => {
-    setEditingId(id);
-    const participantToEdit = participants.find(p => p.id === id);
-    if (participantToEdit) {
-      setNewParticipant(participantToEdit);
-    }
-  }, [participants]);
-
-  const handleSaveEdit = useCallback(async () => {
-    try {
-      const updatedParticipants = participants.map(p =>
-        p.id === editingId ? { ...newParticipant, id: p.id } : p
-      );
-
-      const participantToSave = updatedParticipants.find(p => p.id === editingId);
-      if (participantToSave) {
-        participantSchema.parse(participantToSave);
-        await updateParticipant(participantToSave);
-      }
-
-      setParticipants(updatedParticipants);
-      setEditingId(null);
-      setNewParticipant({ name: "", year: "", club: "", ranking: "" });
-      setErrors({});
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        const newErrors = error.errors.reduce((acc, err) => {
-          acc[err.path.join(".")] = err.message;
-          return acc;
-        }, {} as Record<string, string>);
-        setErrors(newErrors);
-      } else {
-        console.error(error);
-      }
-    }
-  }, [participants, editingId, newParticipant]);
-
-  const handleDeleteParticipant = useCallback(async (id: number) => {
-    try {
-      await deleteParticipant(String(id));
-      const updatedParticipants = participants.filter(participant => participant.id !== id);
-      setParticipants(updatedParticipants);
-    } catch (error) {
-      console.error(error);
-      throw new Error("Failed to delete participant");
-    }
-  }, [participants]);
-  
   const participantsByAlphabet = useMemo(() =>
     participants.length > 0 ?
       [...participants].sort((a, b) => a.name.localeCompare(b.name)) : [],
@@ -136,20 +62,7 @@ function ParticipantsView() {
         </StyledButton>
       </div>
       <div className="group-table">
-        <div className="table-button-container">
-          <StyledButton variant="contained" onClick={handleAddParticipant} disabled={editingId ? true : false}>
-            Add New
-          </StyledButton>
-        </div>
-        <ParticipantsTable
-          participants={participantsByAlphabet}
-          onEditParticipant={handleEditParticipant}
-          onDeleteParticipant={handleDeleteParticipant}
-          editingId={editingId}
-          newParticipant={newParticipant}
-          onInputChange={handleInputChange}
-          onSaveEdit={handleSaveEdit}
-          errors={errors} />
+        <ParticipantsTable inicialParticipants={participantsByAlphabet} />
       </div>
     </>
   );
